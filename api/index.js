3fd56ef6036fb4ba9a295d11cce1ed6bf9b48c4b
cglunt <cglunt11@gmail.com>
2026-01-15 18:31:07 -0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
-import { initializeApp } from "../../dist/server/app.js";
+import { initializeApp } from "../dist/server/app.js";
 
 let appPromise = null;
 
-export default async (req, res) => {
-  if (!appPromise) {
-    appPromise = initializeApp();
+export default async function handler(req, res) {
+  try {
+    if (!appPromise) {
+      appPromise = initializeApp();
+    }
+
+    const app = await appPromise;
+    return app(req, res);
+  } catch (err) {
+    console.error("SERVERLESS HANDLER ERROR:", err);
+    appPromise = null;
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-  const app = await appPromise;
-  return app(req, res);
-};
+}
