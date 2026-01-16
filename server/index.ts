9f13799a7d327cb35064141a942891868dbb7a0e
cglunt <cglunt11@gmail.com>
2026-01-15 23:54:55 -0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 
 (async () => {
   try {
-    await registerRoutes(httpServer, app);
+    await registerRoutes(app);
 
     app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
       const status = err.status || err.statusCode || 500;
